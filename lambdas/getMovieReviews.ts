de2177df663ae1ed029ba13132a2c53bd0282368
Yingying Lu <20095278@mail.wit.ie>
2024-03-17 01:24:05 +0000

    lambdas - GET /movies/{movieId}/reviews - Get all the reviews for the specified movie

---
+++ lambdas/getMovieReviews.ts
@@ -0,0 +1,83 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbDocClient = createDDbDocClient();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+    try{
+        console.log("Event: ", event);
+        const parameters = event?.pathParameters;
+        console.log("Paramters:", parameters)
+        const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
+
+        if (!movieId){
+            return {
+                statusCode: 404,
+                headers: {
+                    "content-type": "application/json",
+                  },
+                  body: JSON.stringify({ Message: "Missing movie Id" }),
+            };
+        }
+
+        const commandOutput = await ddbDocClient.send(
+            new QueryCommand({                              
+                TableName: process.env.TABLE_NAME,      
+                KeyConditionExpression: "MovieId = :m",
+                ExpressionAttributeValues: {
+                    ":m": movieId,
+                },
+            })
+        );
+
+        if(!commandOutput.Items || commandOutput.Items.length === 0){  
+            return {                                                        
+                statusCode: 404,
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({ Message: "Invalid movie Id or no reviews have been written for this movie" }),
+            };
+        }
+
+        const body = {
+            data: commandOutput.Items
+        }
+
+        return{
+            statusCode: 200,
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(body),
+        };
+    } catch (error: any){
+        console.log(JSON.stringify(error));
+            return {
+            statusCode: 500,
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ error }),
+        };
+    }
+};
+
+
+
+
+function createDDbDocClient() {
+    const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+    const marshallOptions = {
+      convertEmptyValues: true,
+      removeUndefinedValues: true,
+      convertClassInstanceToMap: true,
+    };
+    const unmarshallOptions = {
+      wrapNumbers: false,
+    };
+    const translateConfig = { marshallOptions, unmarshallOptions };
+    return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+  }
+
--- lib/ds-assignment1-stack.ts
+++ lib/ds-assignment1-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as custom from "aws-cdk-lib/custom-resources";
 import { generateBatch } from "../shared/util";
 import {movieReviews} from '../seed/movieReviews';
+import * as apig from "aws-cdk-lib/aws-apigateway";
 
 
 export class DsAssignment1Stack extends cdk.Stack {
@@ -19,7 +22,24 @@ export class DsAssignment1Stack extends cdk.Stack {
       tableName: 'MovieReviews',
     });
 
-    new custom.AwsCustomResource(this, 'moviesddbInitData', {
+    // Functions 
+    const getMovieReviewsFn = new lambdanode.NodejsFunction(
+      this,
+      "GetMovieReviewsFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_16_X,
+        entry: `${__dirname}/../lambdas/getMovieReviews.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: movieReviewsTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
+
+    new custom.AwsCustomResource(this, 'reviewsddbInitData', {
       onCreate: {
         service: 'DynamoDB',
         action: 'batchWriteItem',
@@ -29,13 +49,39 @@ export class DsAssignment1Stack extends cdk.Stack {
           },
         },
         physicalResourceId: custom.PhysicalResourceId.of(
-          'moviesddbInitData',
+          'reviewsddbInitData',
         ), //.of(Date.now().toString()),
       },
       policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
         resources: [movieReviewsTable.tableArn],
       }),
     });
+
+    // Permissions 
+    movieReviewsTable.grantReadData(getMovieReviewsFn);
+
+    //REST API
+    const api = new apig.RestApi(this, 'RestApi', {
+      description: 'Movie reviews API',
+      deployOptions: {
+        stageName: 'dev',
+      },
+      defaultCorsPreflightOptions: {
+        allowHeaders: ['Content-Type', 'X-Amz-Date'],
+        allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowCredentials: true,
+        allowOrigins: ['*'],
+      },
+    });
+
+    const moviesEndpoint = api.root.addResource("movies");
+    const movieIdEndpoint = moviesEndpoint.addResource("{movieId}");
+    const movieReviewsEndpoint = movieIdEndpoint.addResource("reviews");
+    movieReviewsEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getMovieReviewsFn, { proxy: true })
+    )
+     
 
   }
 }
